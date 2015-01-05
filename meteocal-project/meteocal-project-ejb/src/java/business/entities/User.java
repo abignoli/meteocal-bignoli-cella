866c0e5ab83df4bef47c5d5908bb6e9044b9eae5
USUARIO <USUARIO@192.168.0.199>
2015-01-05 23:38:57 +0100

    Updated User.java

--- meteocal-project/meteocal-project-ejb/src/java/business/entities/User.java
+++ meteocal-project/meteocal-project-ejb/src/java/business/entities/User.java
@@ -5,7 +5,9 @@
  */
 package business.entities;
 
+import business.shared.data.Group;
 import business.shared.security.PasswordEncrypter;
+import java.util.List;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -16,7 +18,7 @@ import javax.validation.constraints.Pattern;
  */
 
 @Entity
-@Table(name = "users")
+@Table(name = "USER")
 public class User {
     
     @Id
@@ -28,8 +30,43 @@ public class User {
 
     @NotNull(message = "Password cannot be empty")
     private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public boolean isCalendarVisible() {
+        return calendarVisible;
+    }
+
+    public void setCalendarVisible(boolean calendarVisible) {
+        this.calendarVisible = calendarVisible;
+    }
         
-    @Id
     @Pattern(regexp = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
     message = "invalid email")
     @NotNull(message = "Email cannot be empty")
@@ -38,11 +75,23 @@ public class User {
         
     @NotNull(message = "Group name cannot be empty")
     private String groupName;
+    
+//    @ManyToMany
+//    @JoinTable(name="INVITATION",
+//            joinColumns = {@JoinColumn(table = "INVITATION", name = "userID", 
+//                              referencedColumnName = "id")},
+//            inverseJoinColumns = {@JoinColumn(table = "EVENT", name = "eventID", 
+//                              referencedColumnName = "id")})
+//    private List<Event> invitedTo;
+
+//    @OneToMany(mappedBy="userID")
+//    private List<Invitation> invitations;
         
     //TODO default value
     private boolean calendarVisible;
         
     public User() {
+        groupName = Group.USER.getName();
     }
 
     public int getId() {
@@ -60,6 +109,14 @@ public class User {
     public void setUsername() {
         this.username = username;
     }
+    
+//    public List<Event> getInvitedTo() {
+//        return invitedTo;
+//    }
+//
+//    public void setInvitedTo(List<Event> invitedTo) {
+//        this.invitedTo = invitedTo;
+//    }
 
     public boolean isValid() {
         // TODO implement logic
