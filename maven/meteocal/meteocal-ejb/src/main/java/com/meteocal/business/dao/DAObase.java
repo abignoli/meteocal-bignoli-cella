d0d8d97cab044cad96fa3f7f0d2db58cd2ae858d
AndreaBignoli <andrea.bignoli@gmail.com>
2015-01-21 23:55:48 +0100

    Fix for UsernameInUse

--- maven/meteocal/meteocal-ejb/src/main/java/com/meteocal/business/dao/DAObase.java
+++ maven/meteocal/meteocal-ejb/src/main/java/com/meteocal/business/dao/DAObase.java
@@ -15,6 +15,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.Map.Entry;
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -25,11 +26,12 @@ import javax.persistence.criteria.Root;
  * @author Andrea Bignoli
  */
 public abstract class DAObase<T> {
+
     private final static String UNIT_NAME = "MeteoCalPU";
 
     @PersistenceContext(unitName = UNIT_NAME)
     private EntityManager em;
-    
+
     private String tableName;
     private Class<T> databaseEntityClass;
 
@@ -48,13 +50,13 @@ public abstract class DAObase<T> {
     }
 
     public T update(T entity) {
-                // This also persits the entity if it isn't already in the database.
+        // This also persits the entity if it isn't already in the database.
 
         try {
             T result = em.merge(entity);
             em.flush();
             return result;
-            
+
         } catch (Exception e) {
             System.out.println("[ERROR - PERSISTENCE] While running query: " + e.getMessage());
             e.printStackTrace();
@@ -72,15 +74,15 @@ public abstract class DAObase<T> {
             e.printStackTrace();
         }
     }
-    
-     public void detach(T entityToDetach) {
+
+    public void detach(T entityToDetach) {
         try {
             em.detach(entityToDetach);
         } catch (Exception e) {
             System.out.println("[ERROR - PERSISTENCE] While running query: " + e.getMessage());
             e.printStackTrace();
         }
-    }   
+    }
 
     public T find(Object primaryKey) {
         try {
@@ -91,29 +93,30 @@ public abstract class DAObase<T> {
         }
         return null;
     }
-    
+
     public T retrieve(Object primaryKey) throws NotFoundException {
         T result = find(primaryKey);
-        
-        if(result == null)
+
+        if (result == null) {
             throw new NotFoundException(NotFoundException.GENERIC_NOT_FOUND + " - Class: " + databaseEntityClass.getName() + " Key: " + primaryKey.toString());
-        
+        }
+
         return result;
     }
-    
+
     public boolean exists(Object primaryKey) {
         T result = find(primaryKey);
-        
+
         return result != null;
     }
-    
+
     public List<T> findAll() {
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<T> cq = cb.createQuery(databaseEntityClass);
         Root<T> rootEntry = cq.from(databaseEntityClass);
         CriteriaQuery<T> all = cq.select(rootEntry);
         TypedQuery<T> allQuery = em.createQuery(all);
-        
+
         return allQuery.getResultList();
 
     }
@@ -124,13 +127,16 @@ public abstract class DAObase<T> {
         try {
             Query query = em.createQuery(namedQuery);
 
-			// Method that will populate parameters if they are passed not null
+            // Method that will populate parameters if they are passed not null
             // and empty
             if (parameters != null && !parameters.isEmpty()) {
                 insertQueryParameters(query, parameters);
             }
-
-            result = (T) query.getSingleResult();
+            try {
+                result = (T) query.getSingleResult();
+            } catch (NoResultException nre) {
+                result = null;
+            }
 
         } catch (Exception e) {
             System.out.println("Error while running query: " + e.getMessage());
